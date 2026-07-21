d7dc29ca70a0c924abde56ac5720bbe5e61044a6
Yevhenii <stupachenko30@gmail.com>
2026-07-21 12:46:42 +0200

    add1

--- src/test/java/FirstSeleniumHW1Test.java
+++ src/test/java/FirstSeleniumHW1Test.java
@@ -4,7 +4,7 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-public class FirstSeleniumTestHW1 {
+public class FirstSeleniumHW1Test {
 
     WebDriver driver;
     @BeforeMethod
