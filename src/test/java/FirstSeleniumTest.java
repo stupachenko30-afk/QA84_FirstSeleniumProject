f4efa4608ea5c66266ca760624262f58ca985525
Yevhenii <stupachenko30@gmail.com>
2026-07-21 12:08:03 +0200

    add

---
+++ src/test/java/FirstSeleniumTest.java
@@ -0,0 +1,31 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class FirstSeleniumTest {
+                      //before -> setUp
+    WebDriver driver;
+    @BeforeMethod
+    public void setUp(){
+        driver = new ChromeDriver();
+        driver.get("https://www.google.com"); //Основной метод without history
+       // driver.navigate().to("https://www.google.com");
+        driver.navigate().back(); //one page back
+        driver.navigate().forward();
+      //  driver.navigate().refresh();// refresh=reload
+    }
+                       //test "Register new user"
+          @Test
+    public void firstTest(){
+              System.out.println("Google opened");
+
+          }
+                        //after ->tearDown
+    @AfterMethod(enabled = false)
+    public void tearDown(){
+        driver.quit(); // close all tabs and browser
+       // driver.close(); // close only current tab in browser
+    }
+}
---
+++ src/test/java/FirstSeleniumTestHW1.java
@@ -0,0 +1,27 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class FirstSeleniumTestHW1 {
+
+    WebDriver driver;
+    @BeforeMethod
+    public void setUp(){
+        driver = new ChromeDriver();
+        driver.get("https://uakino.best/filmy");
+       // driver.navigate().forward();
+        driver.navigate().back();
+    }
+    @Test
+    public void firstTest(){
+
+        System.out.println("UA kino opened");
+    }
+@AfterMethod //(enabled = false)
+    public void tearDown(){
+        driver.quit();
+
+}
+}
